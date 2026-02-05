a9b22bb75b871f0fb336da09b146c47ce50b6a86
michelangelomancini4 <michelman93@tiscali.it>
2026-02-05 13:39:32 +0100

    feat(frontend): build homepage base layout and booking section

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,22 +10,39 @@ export default function HomePage() {
             <div className={styles.hometitle}>
                 <h1>PRENOTA ORA </h1>
             </div>
-
+            {/* JUMBOTRON SECTION */}
             <div className={styles.jumbotron}>
                 <div className={styles.jumbo_left_box} style={{
                     backgroundImage: `url(${foto1})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
+
                 }}>
                     <h3>foto1</h3>
                 </div>
                 <div className={styles.jumbo_right_box} style={{
                     backgroundImage: `url(${foto2})`,
-                    backgroundSize: "cover",
-                    backgroundPosition: "center",
+
                 }} >
                     <h3>foto2</h3>
                 </div>
+            </div>
+            {/* BOOKING BOX SECTION */}
+            <div className={styles.booking_box_container}>
+                <div className={styles.booking_box} >
+                    <h3>qui andrà la box</h3>
+                </div>
+            </div>
+            {/* CARDS SECTION */}
+            <div className={styles.cards_container}>
+                <div className={styles.first_card}>
+                    <h4>card</h4>
+                </div>
+                <div className={styles.second_card}>
+                    <h4>card</h4>
+                </div>
+                <div className={styles.third_card}>
+                    <h4>card</h4>
+                </div>
+
             </div>
         </div >
     )
